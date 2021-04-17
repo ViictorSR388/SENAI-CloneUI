4c2dfe4133c8cf76a94265c32c92431735102f25
Allan dos Santos <71888075+allan-gh@users.noreply.github.com>
2021-04-16 22:08:51 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import PaginaLogin from './componentes/PaginaLogin'
+import Celslide from './componentes/Celslide'
+
+function App() {
+  return (
+    <div className="App">
+
+      <Celslide/> 
+      <PaginaLogin/>
+
+
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/Celslide/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './style1.css';
+
+export default function Celslide(){
+    return(
+        <div className="instagram-phone">
+            
+                <img className="bg" src="img/phone.png" alt="celular"/>
+
+            
+                <div className="slide">
+                    <img src="" alt="Foto"/>
+                    <img src="img/insta-2.jpg" alt="Foto"/>
+                    <img src="img/insta-3.jpg" alt="Foto"/>
+                    <img src="img/insta-4.jpg" alt="Foto"/>
+                    <img src="img/insta-5.jpg" alt="Foto"/>
+                </div>
+            
+            </div>
+    );
+} 
