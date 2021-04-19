338f006333392d4fa0c082849096104f4073cace
Antonio1711 <71889113+Antonio1711@users.noreply.github.com>
2021-04-19 14:35:40 -0300

    Add files via upload

--- pagina-login/src/paginas/PaginaLogin/index.js
+++ pagina-login/src/paginas/PaginaLogin/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import './styles.css';
+import '../../styles/login/PaginaLogin.scss';
 
-import Celslide from '../../componentes/Celslide';
-import Loginslide from '../../componentes/Loginslide';
+import Celslide from '../../componentes/login/Celslide';
+import Loginslide from '../../componentes/login/Loginslide';
 
 export default function PaginaLogin() {
     return(
